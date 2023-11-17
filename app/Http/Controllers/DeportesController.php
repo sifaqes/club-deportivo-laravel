6afcd87f15bd25bcf6ae426f134d60d4ec4ca8b0
SIFAQES <siphaxzer@gmail.com>
2023-11-17 01:07:22 +0100

    Create model Deportes

---
+++ app/Http/Controllers/DeportesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Deportes;
+use Illuminate\Http\Request;
+
+class DeportesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Deportes $deportes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Deportes $deportes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Deportes $deportes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Deportes $deportes)
+    {
+        //
+    }
+}
