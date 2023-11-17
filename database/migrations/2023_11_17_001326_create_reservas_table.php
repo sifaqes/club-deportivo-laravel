b3f44521db5665569e1a0f8272317dc2075c91b4
SIFAQES <siphaxzer@gmail.com>
2023-11-17 01:13:44 +0100

    Create model Reservas

--- database/migrations/2023_11_17_001326_create_reservas_table.php
+++ database/migrations/2023_11_17_001326_create_reservas_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('deportes', function (Blueprint $table) {
+        Schema::create('reservas', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('descripcion');
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('deportes');
+        Schema::dropIfExists('reservas');
     }
 };
