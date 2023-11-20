aef7fd96dfcd3fa7783933edc100ae26d7ba198a
SIFAQES <siphaxzer@gmail.com>
2023-11-20 03:17:17 +0100

    set seeder

---
+++ database/seeders/SociosSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SociosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        // Socio::factory(5)->create();
+    }
+}
